ffeeb8c81f99989abfac19ff63973ae33916fbc7
Masum Alam <masum.7788alam@gmail.com>
2022-08-09 21:17:18 +0530

    chngaes

--- js/script.js
+++ js/script.js
@@ -149,5 +149,38 @@ $(document).ready(function () {
         
     })
     
+    /*==============counter================*/
+    var a = 0;
+    $(window).scroll(function() {
+
+        var oTop = $('#numCounter').offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+            $('.counter').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+            },
+                {
+
+                duration: 2000,
+                easing: 'swing',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+                });
+            });
+            a = 1;
+        }
+
+    });
+    
     
 })
